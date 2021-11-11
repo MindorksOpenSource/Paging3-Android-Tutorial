ff2fc19fc3017968ae3808a59275e0d0a94fc594
Shams <yasir@ezhire.life>
2021-11-11 18:51:27 +0500

    exception handle and code upgrade

--- app/src/main/java/com/mindorks/example/paging3/data/APIService.kt
+++ app/src/main/java/com/mindorks/example/paging3/data/APIService.kt
@@ -12,7 +12,7 @@ import retrofit2.http.Query
 interface APIService {
 
     @GET("api/users")
-    suspend fun getListData(@Query("page") pageNumber: Int): Response<ApiResponse>
+    suspend fun getListData(@Query("page") pageNumber: Int): ApiResponse
 
     companion object {
 

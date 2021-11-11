76ec12521d2d1f985e9b3a57d42c186cb58bace6
Shams <yasir@ezhire.life>
2021-11-11 18:39:48 +0500

    "paging not working" -> Issue resolved

--- app/src/main/java/com/mindorks/example/paging3/data/response/ApiResponse.kt
+++ app/src/main/java/com/mindorks/example/paging3/data/response/ApiResponse.kt
@@ -5,15 +5,15 @@ import com.squareup.moshi.Json
 
 data class ApiResponse(
     @Json(name = "ad")
-    val ad: Ad,
+    val ad: Ad?,
     @Json(name = "data")
-    val myData: List<Data>,
+    val myData: List<Data>?,
     @Json(name = "page")
-    val page: Int,
+    val page: Int?,
     @Json(name = "per_page")
-    val per_page: Int,
+    val per_page: Int?,
     @Json(name = "total")
-    val total: Int,
+    val total: Int?,
     @Json(name = "total_pages")
-    val total_pages: Int
+    val total_pages: Int?
 )
